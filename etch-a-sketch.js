867073ed2764323830a4bb686ec03e24c9acfa6a
declan-rs <declan.rsg@gmail.com>
2025-12-04 16:00:10 +1100

    Add 16x16 grid using flexbox

---
+++ etch-a-sketch.js
@@ -0,0 +1,14 @@
+const container = document.querySelector("#container");
+
+// Create grid
+for(i = 0; i < 16; i++) {
+    let row = document.createElement("div");
+    row.className = "row";
+    container.appendChild(row);
+
+    for(j = 0; j < 16; j++) {
+        let cell = document.createElement("div");
+        cell.className = "cell";
+        row.appendChild(cell);
+    }
+}
