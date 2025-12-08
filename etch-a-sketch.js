fa3142a4b325b7dcd5de4c53643c598285231689
declan-rs <declan.rsg@gmail.com>
2025-12-08 14:55:59 +1100

    Add reset button and grid size choice

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,19 +1,46 @@
 const container = document.querySelector("#container");
+// Create first grid with default 16x16
+createNewGrid(16);
 
 function colourCell() {
     this.classList.add("coloured");
 }
 
-// Create grid
-for(i = 0; i < 16; i++) {
-    let row = document.createElement("div");
-    row.className = "row";
-    container.appendChild(row);
+function reset() {
+    let newSize = prompt("What size do you want the grid? (max 100)");
+    if(!Number.isInteger(+newSize)) {
+        reset();
+        return;
+    } else if(newSize.trim(" ") === "") {
+        reset();
+        return;
+    }else if(newSize > 100) {
+        newSize = 100;
+    }
 
-    for(j = 0; j < 16; j++) {
-        let cell = document.createElement("div");
-        cell.className = "cell";
-        row.appendChild(cell);
-        cell.addEventListener('mouseover', colourCell);
+    removeGrid();
+    createNewGrid(newSize);
+}
+
+document.getElementById('reset').addEventListener('click', reset);
+
+function removeGrid() {
+    while(container.lastChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+function createNewGrid(size) {
+    for(let i = 0; i < size; i++) {
+        let row = document.createElement("div");
+        row.className = "row";
+        container.appendChild(row);
+
+        for(let j = 0; j < size; j++) {
+            let cell = document.createElement("div");
+            cell.className = "cell";
+            row.appendChild(cell);
+            cell.addEventListener('mouseover', colourCell);
+        }
     }
 }
