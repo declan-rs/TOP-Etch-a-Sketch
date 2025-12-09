4b932ec6bb7f07740fe3cf86ddcd2144da0fd4c8
declan-rs <declan.rsg@gmail.com>
2025-12-09 17:11:19 +1100

    Add darkening mode

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,29 +1,62 @@
 // Elements
-const container = document.querySelector("#container");
-const resetButton = document.getElementById("resetButton");
-const sizeSlider = document.getElementById("sizeSlider");
-const sizeValue = document.getElementById("sizeValue");
+const grid = document.getElementById("grid");
+const blockButton = document.getElementById("block-button");
+const darkeningButton = document.getElementById("darkening-button");
+const rainbowButton = document.getElementById("rainbow-button");
+const resetButton = document.getElementById("reset-button");
+const sizeSlider = document.getElementById("size-slider");
+const sizeValue = document.getElementById("size-value");
+
+// Settings
+let mode = "block";
 
 // Create first grid with default 16x16
-createNewGrid(16);
+createNewGrid(10);
 
 function colourCell() {
+    if(mode === "darkening") {
+        let curOpacity = parseFloat(this.style.opacity);
+        if(!this.classList.contains("coloured")) {
+            // First time coloured
+            
+            this.style.backgroundColor = "black";
+            this.style.opacity = 0.1;
+        } else if (curOpacity < 1){
+            // Already coloured, increase opacity
+            this.style.opacity = curOpacity + 0.1;
+        }
+    } else {
+        this.style.backgroundColor = "black";
+    }
     this.classList.add("coloured");
 }
 
 function reset() {
-    let newSize = document.getElementById("sizeSlider").value;
+    let newSize = sizeSlider.value;
 
     removeGrid();
     createNewGrid(newSize);
 }
 
+function changeBlockMode() {
+    mode = "block";
+    reset();
+}
+
+function changeDarkeningMode() {
+    mode = "darkening";
+    reset();
+}
+
+blockButton.addEventListener("click", changeBlockMode);
+darkeningButton.addEventListener("click", changeDarkeningMode);
 resetButton.addEventListener("click", reset);
 sizeSlider.addEventListener("input", () => sizeValue.textContent = sizeSlider.value + "x" + sizeSlider.value);
 
+
 function removeGrid() {
-    while(container.lastChild) {
-        container.removeChild(container.lastChild);
+    while(grid.lastChild) {
+        grid.removeChild(grid.lastChild);
     }
 }
 
@@ -31,7 +64,7 @@ function createNewGrid(size) {
     for(let i = 0; i < size; i++) {
         let row = document.createElement("div");
         row.className = "row";
-        container.appendChild(row);
+        grid.appendChild(row);
 
         for(let j = 0; j < size; j++) {
             let cell = document.createElement("div");
